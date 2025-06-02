0b8167591267d573c7601a38dc53e59ccd6bf577
Ravikiran <ravi@DESKTOP-8AJ9JMC.localdomain>
2025-06-02 15:49:22 +0530

    mess

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ const PDFDocument = require("pdfkit");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+// FIX: support up to 10mb POSTs
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 5000;
 
